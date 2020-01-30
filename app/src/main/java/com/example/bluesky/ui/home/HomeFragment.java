4ccad68b1eb61fda3ea74944a82573e8379e25f9
Taugust8 <55388016+Taugust8@users.noreply.github.com>
2020-01-30 08:39:02 +0100

    commit 1

--- app/src/main/java/com/example/bluesky/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/bluesky/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -23,11 +24,11 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final ListView listView = root.findViewById(R.id.lesSons);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
             }
         });
         return root;
