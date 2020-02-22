8b4902d80138555d6a440a15d6b3026da36c10c6
Taugust8 <55388016+Taugust8@users.noreply.github.com>
2020-02-22 16:27:42 +0100

    update

--- app/src/main/java/com/example/bluesky/MainActivity.java
+++ app/src/main/java/com/example/bluesky/MainActivity.java
@@ -2,60 +2,27 @@ package com.example.bluesky;
 
 import android.os.Bundle;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+
 import com.example.bluesky.ui.dashboard.DashboardFragment;
 import com.example.bluesky.ui.home.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import android.view.View;
-import android.view.ViewGroup;
+
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import android.net.Uri;
-import android.content.ContentResolver;
-import android.database.Cursor;
-import android.text.Layout;
-import android.util.JsonReader;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ListView;
-import java.io.*;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.Charset;
-import org.json.JSONArray;
-import android.graphics.Color;
 
-import org.json.JSONObject;
 
 import android.media.MediaPlayer;
-import android.view.LayoutInflater;
-import java.io.IOException;
-import android.widget.TextView;
 
-import android.graphics.Bitmap;
-
-import android.graphics.BitmapFactory;
-
-public class MainActivity extends AppCompatActivity {
-
-
+public class MainActivity extends AppCompatActivity
+{
 
     final private static String url = "http://webinfo.iutmontp.univ-montp2.fr/~chambaudM/BlueSky-JS-Project/";
-    private static MediaPlayer lecteur=null;
+    private static MediaPlayer lecteur = null;
     private static Song sonEnCours = null;
 
 
@@ -64,15 +31,10 @@ public class MainActivity extends AppCompatActivity {
     {
         setTheme(R.style.AppThemeReady);
         super.onCreate(savedInstanceState);
-        MainActivity.lecteur=new MediaPlayer();
+        MainActivity.lecteur = new MediaPlayer();
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        Button btnPlay = findViewById(R.id.btn1);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
@@ -80,28 +42,32 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public static void  playSong(final Integer index)
+
+    public static void playSong(final Integer index)
     {
         try
         {
-            Song unSon= HomeFragment.getSongList().get(index);
-            System.out.println(unSon.getUrl());
+            Song unSon = HomeFragment.getSongList().get(index);
             System.out.println(MainActivity.sonEnCours);
-            if(MainActivity.sonEnCours==null||!unSon.getUrl().equals(MainActivity.sonEnCours.getUrl()))
+            if(MainActivity.sonEnCours == null|| !unSon.getUrl().equals(MainActivity.sonEnCours.getUrl()))
             {
 
                 MainActivity.lecteur.stop();
-                MainActivity.lecteur=new MediaPlayer();
+                MainActivity.lecteur = new MediaPlayer();
                 MainActivity.lecteur.setDataSource(unSon.getUrl());
                 MainActivity.lecteur.prepare();
-                MainActivity.lecteur.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                    @Override
-                    public void onPrepared(MediaPlayer mp) {
+                MainActivity.lecteur.setOnPreparedListener(new MediaPlayer.OnPreparedListener()
+                {
+                    public void onPrepared(MediaPlayer mp)
+                    {
                         MainActivity.lecteur.start();
                     }
                 });
             }
-            MainActivity.sonEnCours=unSon;
+
+            MainActivity.sonEnCours = unSon;
+
+            //DashboardFragment.getProgressBar().setMax(MainActivity.getLecteur().getDuration()/1000);
 
         }
         catch(Exception e)
@@ -109,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
             System.out.println(e);
         }
     }
+
     public static MediaPlayer getLecteur() {
         return lecteur;
     }
@@ -120,4 +87,6 @@ public class MainActivity extends AppCompatActivity {
     public static String getUrl() {
         return url;
     }
+
+
 }
--- app/src/main/java/com/example/bluesky/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/bluesky/ui/dashboard/DashboardFragment.java
@@ -1,18 +1,18 @@
 package com.example.bluesky.ui.dashboard;
 
 import android.graphics.Bitmap;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.os.Handler;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.android.volley.RequestQueue;
@@ -23,32 +23,113 @@ import com.android.volley.toolbox.Volley;
 import com.example.bluesky.MainActivity;
 import com.example.bluesky.R;
 import com.example.bluesky.Song;
-import com.example.bluesky.ui.home.HomeFragment;
+import com.example.bluesky.Utilities;
 
-public class DashboardFragment extends Fragment {
+public class DashboardFragment extends Fragment
+{
 
     private DashboardViewModel dashboardViewModel;
     private TextView titre = null;
-    private ImageView cover=null;
+    private ImageView cover = null;
+
+    private Button btnPlay;
+    private Button btnPause;
+    private Button btnNext;
+    private Button btnPrevious;
+
+    //private Handler mHandler = new Handler();
+    //private Utilities utils;
+
+    //private static SeekBar progressBar;
+    private TextView songCurrentDurationLabel;
+    private TextView songTotalDurationLabel;
 
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        dashboardViewModel = ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        this.titre=root.findViewById(R.id.titre);
-        this.cover=root.findViewById(R.id.covertArt);
+
+        this.btnPlay = (Button) root.findViewById(R.id.btn1);
+        this.btnPause = (Button) root.findViewById(R.id.btn2);
+        this.btnNext = (Button) root.findViewById(R.id.btn3);
+        this.btnPrevious = (Button) root.findViewById(R.id.btn0);
+        this.songCurrentDurationLabel = root.findViewById(R.id.songTotalDurationLabel);
+        this.songTotalDurationLabel = root.findViewById(R.id.songTotalDurationLabel);
+        //this.progressBar = root.findViewById(R.id.seekBar);
+
+
+        //this.utils = new Utilities();
+
+
+        btnPause.setVisibility(View.INVISIBLE);
+        //btnPlay.setVisibility(View.INVISIBLE);
+        //btnNext.setVisibility(View.INVISIBLE);
+        //btnPrevious.setVisibility(View.INVISIBLE);
+
+
+        this.btnPlay.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                MainActivity.getLecteur().start();
+                btnPlay.setVisibility(View.INVISIBLE);
+                btnPause.setVisibility(View.VISIBLE);
+            }
+
+        });
+
+        this.btnPause.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                MainActivity.getLecteur().pause();
+                btnPlay.setVisibility(View.VISIBLE);
+                btnPause.setVisibility(View.INVISIBLE);
+            }
+
+        });
+
+        this.btnNext.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                int position = MainActivity.getSonEnCours().getId();
+                MainActivity.playSong(position);
+                setInfosSon();
+            }
+
+        });
+
+        this.btnPrevious.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                MainActivity.playSong(MainActivity.getSonEnCours().getId() - 2);
+                setInfosSon();
+            }
+
+        });
+
+
+        this.titre = root.findViewById(R.id.titre);
+        this.cover = root.findViewById(R.id.covertArt);
         this.setInfosSon();
         return root;
     }
 
     public void setInfosSon()
     {
+        btnPlay.setVisibility(View.INVISIBLE);
+        btnPause.setVisibility(View.VISIBLE);
         Song unSon= MainActivity.getSonEnCours();
-        if(unSon!=null)
+        if(unSon != null)
         {
-            if(unSon.getImage()==null)
+            if(unSon.getImage() == null)
             {
                 RequestQueue queue = Volley.newRequestQueue(getContext());
                 ImageRequest imageRequest = new ImageRequest( unSon.getCoverArtUrl(),
@@ -59,7 +140,8 @@ public class DashboardFragment extends Fragment {
                                 DashboardFragment.this.setInfosSon();
                             }
                         }, 0, 0, ImageView.ScaleType.CENTER_CROP,null,
-                        new Response.ErrorListener() {
+                        new Response.ErrorListener()
+                        {
                             public void onErrorResponse(VolleyError error) {
                                 System.out.println(error);
                             }
@@ -71,7 +153,60 @@ public class DashboardFragment extends Fragment {
         }
     }
 
+/*
+
+    private Runnable mUpdateTimeTask = new Runnable() {
+        public void run() {
+            long totalDuration = MainActivity.getLecteur().getDuration();
+            long currentDuration = MainActivity.getLecteur().getCurrentPosition();
+
+            // Displaying Total Duration time
+            songTotalDurationLabel.setText(""+utils.milliSecondsToTimer(totalDuration));
+            // Displaying time completed playing
+            songCurrentDurationLabel.setText(""+utils.milliSecondsToTimer(currentDuration));
+
+            // Updating progress bar
+            int progress = (int)(utils.getProgressPercentage(currentDuration, totalDuration));
+            //Log.d("Progress", ""+progress);
+            progressBar.setProgress(progress);
+
+            // Running this thread after 100 milliseconds
+            mHandler.postDelayed(this, 100);
+        }
+    };
 
 
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
+
+    }
+
+    public void updateProgressBar() {
+        mHandler.postDelayed(mUpdateTimeTask, 100);
+    }
+
+
+
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // remove message Handler from updating progress bar
+        mHandler.removeCallbacks(mUpdateTimeTask);
+    }
+
+
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        mHandler.removeCallbacks(mUpdateTimeTask);
+        int totalDuration = MainActivity.getLecteur().getDuration();
+        int currentPosition = utils.progressToTimer(seekBar.getProgress(), totalDuration);
+
+        // forward or backward to certain seconds
+        MainActivity.getLecteur().seekTo(currentPosition);
+
+        // update timer progress again
+        updateProgressBar();
+    }
+
+    public static SeekBar getProgressBar()
+    {
+        return progressBar;
+    }*/
 
 }
